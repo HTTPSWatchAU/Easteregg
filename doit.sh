6290def2017383fd00c7b8e33b5d2c08c508de0f
Phillip Smith <fukawi2@gmail.com>
2015-04-27 13:00:50 +1000

    initial commit of doit.sh

---
+++ doit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+### yes this is a nasty hack, but it will work for now.
+
+set -e
+
+cd "$(dirname $0)"
+
+./runjobs.py $1
+./generatesummary.py -t -r web/twbankssl.xml > web/ssltest.json
